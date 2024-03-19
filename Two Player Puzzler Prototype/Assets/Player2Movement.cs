447163c3710824c295f384a69d345b97e4c30a40
Erika Becker <97013344+eribecke@users.noreply.github.com>
2024-03-19 19:52:10 -0400

    added gravity mechanic

---
+++ Two Player Puzzler Prototype/Assets/Player2Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.right * 10, ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.left * 10, ForceMode2D.Force);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 5, ForceMode2D.Impulse);
+        }
+    }
+}
